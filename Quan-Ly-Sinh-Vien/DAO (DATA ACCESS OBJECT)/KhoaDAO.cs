fe8affb08ecc50ea3f70fdf21976e21d130d9da0
Nguyễn Anh Tuấn <nganhtuan5847@gmail.com>
2025-09-13 22:45:55 +0700

    docs : dao khoa

--- Quan-Ly-Sinh-Vien/DAO (DATA ACCESS OBJECT)/KhoaDAO.cs
+++ Quan-Ly-Sinh-Vien/DAO (DATA ACCESS OBJECT)/KhoaDAO.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Quan_Ly_Sinh_Vien.DTO__DATA_TRANSFER_OBJECT_;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Quan_Ly_Sinh_Vien.DAO__DATA_ACCESS_OBJECT_
 {
-    internal class KhoaDAO
+    public class KhoaDAO
     {
+        public static List<Khoa> GetAll()
+        {
+            List<Khoa> list = new List<Khoa>();
+            try
+            {
+                DataTable dt = DataProvider.LoadCSDL("SELECT * FROM Khoa");
+                foreach (DataRow row in dt.Rows)
+                {
+                    list.Add(new Khoa
+                    {
+                        MaKhoa = row["MaKhoa"].ToString(),
+                        TenKhoa = row["TenKhoa"].ToString()
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi KhoaDAO.GetAll: " + ex.Message);
+            }
+            return list;
+        }
     }
 }
