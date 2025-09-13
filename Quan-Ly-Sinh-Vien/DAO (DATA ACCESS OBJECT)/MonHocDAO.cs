65d29e9b595bb4e19445d63c857c7336c3299bb4
Nguyễn Anh Tuấn <nganhtuan5847@gmail.com>
2025-09-13 22:48:12 +0700

    docs : mon hoc dao

--- Quan-Ly-Sinh-Vien/DAO (DATA ACCESS OBJECT)/MonHocDAO.cs
+++ Quan-Ly-Sinh-Vien/DAO (DATA ACCESS OBJECT)/MonHocDAO.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using Quan_Ly_Sinh_Vien.DTO__DATA_TRANSFER_OBJECT_;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Quan_Ly_Sinh_Vien.DAO__DATA_ACCESS_OBJECT_
 {
-    internal class MonHocDAO
+   public class MonHocDAO
     {
+        public static List<MonHoc> GetAll()
+        {
+            List<MonHoc> list = new List<MonHoc>();
+            try
+            {
+                DataTable dt = DataProvider.LoadCSDL("SELECT * FROM MonHoc");
+                foreach (DataRow row in dt.Rows)
+                {
+                    list.Add(new MonHoc
+                    {
+                        MaMH = row["MaMH"].ToString(),
+                        TenMH = row["TenMH"].ToString(),
+                        SoTiet = Convert.ToInt32(row["SoTiet"])
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi MonHocDAO.GetAll: " + ex.Message);
+            }
+            return list;
+        }
     }
 }
