4fee6778e03878856c451a88533f056e9b270809
Nguyễn Anh Tuấn <nganhtuan5847@gmail.com>
2025-09-13 22:44:44 +0700

    docs : dao ket qua

--- Quan-Ly-Sinh-Vien/DAO (DATA ACCESS OBJECT)/KetQuaDAO.cs
+++ Quan-Ly-Sinh-Vien/DAO (DATA ACCESS OBJECT)/KetQuaDAO.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Quan_Ly_Sinh_Vien.DTO__DATA_TRANSFER_OBJECT_;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Quan_Ly_Sinh_Vien.DAO__DATA_ACCESS_OBJECT_
 {
-    internal class KetQuaDAO
+    public class KetQuaDAO
     {
+        public static List<KetQua> GetAll()
+        {
+            List<KetQua> list = new List<KetQua>();
+            try
+            {
+                DataTable dt = DataProvider.LoadCSDL("SELECT * FROM KetQua");
+                foreach (DataRow row in dt.Rows)
+                {
+                    list.Add(new KetQua
+                    {
+                        MaSV = row["MaSV"].ToString(),
+                        MaMH = row["MaMH"].ToString(),
+                        DiemLan1 = (float)(row["DiemLan1"] == DBNull.Value ? null : (decimal?)Convert.ToDecimal(row["DiemLan1"])),
+                        DiemThiLai = (float)(row["DiemThiLai"] == DBNull.Value ? null : (decimal?)Convert.ToDecimal(row["DiemThiLai"]))
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi KetQuaDAO.GetAll: " + ex.Message);
+            }
+            return list;
+        }
     }
 }
